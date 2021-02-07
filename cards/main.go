344497d1314e5624985ca9a0d7c1cc5447182e1c
h1m <himanshu.yadav643@gmail.com>
2021-02-07 13:30:50 +0530

    understood functions, slice and for loops

--- cards/main.go
+++ cards/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// var card string = "Ace of Spades"
+	// card := "Ace of Spades" // better way to assign variables
+	// card := newCard()
+	// card = "Five of Diamonds"
+
+	cards := []string{"Ace of Spades", newCard()} // this is how we create a slice - need to mention the type as it will contain only one type
+	cards = append(cards, "Six of Spades")        // this is how we append to the slice - it will create a new slice, will not modify it
+	// fmt.Println(cards) // for printing
+
+	for i, card := range cards { // this is how we iterate over a slice
+		fmt.Println(i, card)
+	}
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
