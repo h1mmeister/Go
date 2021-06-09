5d65e92216512f3d30ff7391802aa23f96b3c80c
h1m <himanshu.yadav643@gmail.com>
2021-06-09 10:03:55 +0530

    working on pointers

--- syntax/main.go
+++ syntax/main.go
@@ -15,6 +15,13 @@ type User struct {
 
 func main() {
 
+	var myValue string
+	myValue = "Green"
+
+	log.Println("myValue is", myValue)
+	changeUsingPointers(&myValue)
+	log.Println("after using the function", myValue)
+
 	user := User {
 		FirstName: "Himanshu",
 		LastName: "Yadav",
@@ -22,4 +29,10 @@ func main() {
 
 	log.Println(user.FirstName)
 
+}
+
+func changeUsingPointers(s *string) {
+	log.Println(s, *s)
+	newValue := "Red"
+	*s = newValue
 }
