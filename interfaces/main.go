0ff208a230a768e83f7e35dc15a6cf9bd2207712
h1m <himanshu.yadav643@gmail.com>
2021-06-29 14:00:53 +0530

    working on interfaces

---
+++ interfaces/main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+type Animal interface {
+	Says() string
+	NumberOfLegs() int
+}
+
+type Dog struct {
+	Name  string
+	Breed string
+}
+
+type Gorilla struct {
+	Name          string
+	Color         string
+	NumberofTeeth string
+}
+
+func main() {
+
+	dog := Dog{
+		Name:  "cherry",
+		Breed: "pitbull",
+	}
+
+	PrintInfo(&dog)
+}
+
+func PrintInfo(a Animal) {
+	fmt.Println("This animal says", a.Says(), "and has", a.NumberOfLegs(), "legs")
+}
+
+func (d *Dog) Says() string {
+	return "woof"
+}
+
+func (d *Dog) NumberOfLegs() int {
+	return 4
+}
