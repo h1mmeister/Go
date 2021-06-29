a65de0e17ac1a107010239eb3c2f23392ae2144d
h1m <himanshu.yadav643@gmail.com>
2021-06-29 14:46:48 +0530

    functions and handlers

--- website/main.go
+++ website/main.go
@@ -5,17 +5,33 @@ import (
 	"net/http"
 )
 
+// home page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	n, err := fmt.Fprintf(w, "This is my first attempt to create a website in Go! Here is the Home Page.")
+	// check if there is any error
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
+}
+
+// about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := aboutValues(1,1)
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the %dnd page called About!", sum))
+
+}
+
+func aboutValues(x int, y int) int {
+	return x+y
+}
+
 
 func main() {
-	// this method takes an inline function with a response writer and a pointer to the request
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		n, err := fmt.Fprintf(w, "This is my first attempt to create a website in Go!")
-		// check if there is any error
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
-	})
+	
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
 	// to serve requests via browser
 	_ = http.ListenAndServe(":7070", nil)
 
