8898e8d7cb0130ec72a8f5426d42b3e103843cf0
h1m <himanshu.yadav643@gmail.com>
2021-07-01 14:37:11 +0530

    error handling and rendering html files

--- website/main.go
+++ website/main.go
@@ -1,60 +1,43 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
+const portNumber = ":7070"
+
 // home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
-	n, err := fmt.Fprintf(w, "This is my first attempt to create a website in Go! Here is the Home Page.")
-	// check if there is any error
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
+	renderTemplate(w, "home.html")
 }
 
 // about page handler
 func About(w http.ResponseWriter, r *http.Request) {
-	sum := aboutValues(1,1)
-	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the %dnd page called About!", sum))
-
+	renderTemplate(w, "about.html")
 }
 
-func aboutValues(x int, y int) int {
-	return x+y
-}
-
-func Divide(w http.ResponseWriter, r *http.Request) {
-	d, err := divideValues(10.0, 0.0)
+// this will help in rendering the templates
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
-		fmt.Fprintf(w, "can not divide by 0.0")
+		fmt.Println("error parsing template", err)
 		return
 	}
-
-	fmt.Fprint(w, fmt.Sprintf("Division result equals %f", d))
 }
 
-func divideValues(x, y float32) (float32, error) {
-	if y <= 0 {
-		err := errors.New("can not divide by 0 or less than 0")
-		return 0, err
-	} else {
-		result := x / y
-		return result, nil
-	}
-}
+
 
 func main() {
 	
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
-	http.HandleFunc("/divide", Divide)
+
 
 	// to serve requests via browser
-	_ = http.ListenAndServe(":7070", nil)
+	_ = http.ListenAndServe(portNumber, nil)
 
 	}
 
